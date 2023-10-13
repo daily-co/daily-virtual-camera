9660c14665d5bfd264216ebe40cfb4f43ddf8c3a
Aleix Conchillo Flaqué <aleix@daily.co>
2023-10-13 13:33:09 -0700

    really use UserDefaults instead of Info.plist

--- cam/UserDefaults+Extensions.swift
+++ cam/UserDefaults+Extensions.swift
@@ -8,9 +8,19 @@ extension UserDefaults {
         static let gStreamerPipeline: String = "gStreamerPipeline"
     }
 
+    enum VirtualCameraDefaults {
+        static let videoWidth: Int32 = 1280
+        static let videoHeight: Int32 = 720
+        static let videoFramerate: Int32 = 30
+        static let gStreamerPipeline: String = "videotestsrc pattern=ball ! capsfilter caps=video/x-raw,width=1280,height=720"
+    }
     var videoWidth: Int32 {
         get {
-            self.value(forKey: VirtualCameraKeys.videoWidth) as! Int32
+            if let value = self.value(forKey: VirtualCameraKeys.videoWidth) {
+                return value as! Int32
+            }
+            self.set(VirtualCameraDefaults.videoWidth, forKey: VirtualCameraKeys.videoWidth)
+            return VirtualCameraDefaults.videoWidth
         }
         set {
             self.set(newValue, forKey: VirtualCameraKeys.videoWidth)
@@ -19,7 +29,11 @@ extension UserDefaults {
 
     var videoHeight: Int32 {
         get {
-            self.value(forKey: VirtualCameraKeys.videoHeight) as! Int32
+            if let value = self.value(forKey: VirtualCameraKeys.videoHeight) {
+                return value as! Int32
+            }
+            self.set(VirtualCameraDefaults.videoHeight, forKey: VirtualCameraKeys.videoHeight)
+            return VirtualCameraDefaults.videoHeight
         }
         set {
             self.set(newValue, forKey: VirtualCameraKeys.videoHeight)
@@ -28,7 +42,11 @@ extension UserDefaults {
 
     var videoFramerate: Int32 {
         get {
-            self.value(forKey: VirtualCameraKeys.videoFramerate) as! Int32
+            if let value = self.value(forKey: VirtualCameraKeys.videoFramerate) {
+                return value as! Int32
+            }
+            self.set(VirtualCameraDefaults.videoFramerate, forKey: VirtualCameraKeys.videoFramerate)
+            return VirtualCameraDefaults.videoFramerate
         }
         set {
             self.set(newValue, forKey: VirtualCameraKeys.videoFramerate)
@@ -37,7 +55,11 @@ extension UserDefaults {
 
     var gStreamerPipeline: String {
         get {
-            self.value(forKey: VirtualCameraKeys.gStreamerPipeline) as! String
+            if let value = self.value(forKey: VirtualCameraKeys.gStreamerPipeline) {
+                return value as! String
+            }
+            self.set(VirtualCameraDefaults.gStreamerPipeline, forKey: VirtualCameraKeys.gStreamerPipeline)
+            return VirtualCameraDefaults.gStreamerPipeline
         }
         set {
             self.set(newValue, forKey: VirtualCameraKeys.gStreamerPipeline)
